0434cb1a1849805acb4d6bdcd4fdbd5cb821cdf6
Ulugbek Abduloev <beka.eco.95@gmail.com>
2019-12-04 19:38:23 +0800

    [ADD]
    onChange form functionality

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Wrapper } from './../elements/Wrapper'
 
 export default function Add() {
+
+    const [todo, setTodo] = useState('')
+
+    const submitHandle = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <Wrapper vertical>
-            <form>
-                <input placeholder="Enter task" />
+            <form onSubmit={submitHandle}>
+                <input 
+                    placeholder="Enter task"
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                />
                 <button>Add Task</button>
             </form>
         </Wrapper>
